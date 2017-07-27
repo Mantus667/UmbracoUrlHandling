d4b42237851626c0a6e18f9147ac482e74682fa7
David Brendel <mantus667@googlemail.com>
2017-07-27 15:51:51 +0200

    Added segment provider
    Added segment provider for real estate

---
+++ src/UmbracoUrlHandling/SegmentProvider/ImmoUrlSegmentProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Strings;
+
+namespace UmbracoUrlHandling.SegmentProvider
+{
+    public class ImmoUrlSegmentProvider : IUrlSegmentProvider
+    {
+        /// <summary>
+        /// The provider.
+        /// </summary>
+        private readonly IUrlSegmentProvider _provider = new DefaultUrlSegmentProvider();
+
+        public string GetUrlSegment(IContentBase content)
+        {
+            return GetUrlSegment(content, CultureInfo.CurrentCulture);
+        }
+
+        public string GetUrlSegment(IContentBase content, CultureInfo culture)
+        {
+            if (content.GetContentType().Alias != "realEstate")
+            {
+                return null;
+            }
+
+            var segment = _provider.GetUrlSegment(content);
+            return $"{content.GetValue("type")}-{content.GetValue<string>("city")}-{segment}".ToUrlSegment();
+        }
+    }
+}
---
+++ src/UmbracoUrlHandling/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Strings;
+using UmbracoUrlHandling.SegmentProvider;
+
+namespace UmbracoUrlHandling
+{
+    public class Startup : IApplicationEventHandler
+    {
+        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+        }
+
+        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            UrlSegmentProviderResolver.Current.InsertType<ImmoUrlSegmentProvider>(0);
+        }
+
+        public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+        }
+    }
+}
