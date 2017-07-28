698d492fe090249e2b48fcc518393abbb25eaa5d
David Brendel <mantus667@googlemail.com>
2017-07-28 07:31:06 +0200

    Added comments for ImmoUrlSegmentProvider
    Added comments for ImmoUrlSegmentProvider

--- src/UmbracoUrlHandling/SegmentProvider/ImmoUrlSegmentProvider.cs
+++ src/UmbracoUrlHandling/SegmentProvider/ImmoUrlSegmentProvider.cs
@@ -5,19 +5,43 @@ using Umbraco.Core.Strings;
 
 namespace UmbracoUrlHandling.SegmentProvider
 {
-    public class ImmoUrlSegmentProvider : IUrlSegmentProvider
+	/// <summary>
+	/// Url segment provider for real estates
+	/// </summary>
+	/// <seealso cref="Umbraco.Core.Strings.IUrlSegmentProvider" />
+	public class ImmoUrlSegmentProvider : IUrlSegmentProvider
     {
         /// <summary>
         /// The provider.
         /// </summary>
         private readonly IUrlSegmentProvider _provider = new DefaultUrlSegmentProvider();
 
-        public string GetUrlSegment(IContentBase content)
+		/// <summary>
+		/// Gets the default url segment for a specified content.
+		/// </summary>
+		/// <param name="content">The content.</param>
+		/// <returns>
+		/// The url segment.
+		/// </returns>
+		public string GetUrlSegment(IContentBase content)
         {
             return GetUrlSegment(content, CultureInfo.CurrentCulture);
         }
 
-        public string GetUrlSegment(IContentBase content, CultureInfo culture)
+		/// <summary>
+		/// Gets the url segment for a specified content and culture.
+		/// </summary>
+		/// <param name="content">The content.</param>
+		/// <param name="culture">The culture.</param>
+		/// <returns>
+		/// The url segment.
+		/// </returns>
+		/// <remarks>
+		/// This is for when Umbraco is capable of managing more than one url
+		/// per content, in 1-to-1 multilingual configurations. Then there would be one
+		/// url per culture.
+		/// </remarks>
+		public string GetUrlSegment(IContentBase content, CultureInfo culture)
         {
             if (content.GetContentType().Alias != "realEstate")
             {
